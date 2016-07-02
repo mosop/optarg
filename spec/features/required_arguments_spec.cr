15a4162ac043ad1f5fb4c7d51d3842b8155ef43f
mosop <mosop@a.b.com>
2016-07-02 10:05:41 +0900

    change for README updates

--- spec/features/required_arguments_spec.cr
+++ spec/features/required_arguments_spec.cr
@@ -1,11 +1,11 @@
 require "../spec_helper"
 
 module Optarg::RequiredArgumentsFeature
-  class Birthday < Optarg::Model
-    string "--when", required: true
+  class Profile < Optarg::Model
+    string "--birth", required: true
   end
 
   it "Required Arguments" do
-    expect_raises(Optarg::RequiredError) { Birthday.parse %w() }
+    expect_raises(Optarg::RequiredError) { Profile.parse %w() }
   end
 end
