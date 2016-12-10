f5f859bb50ffe873698747efe047b8285d35a280
mosop <mosop@users.noreply.github.com>
2016-12-10 11:19:07 +0900

    clean

--- src/optarg/completion_generators/zsh.cr
+++ src/optarg/completion_generators/zsh.cr
@@ -1,12 +1,4 @@
 class Optarg::CompletionGenerators
   class Zsh < Base
-    def make_header
-      super
-      if first?
-        @header << <<-EOS
-        setopt localoptions ksharrays
-        EOS
-      end
-    end
   end
 end
